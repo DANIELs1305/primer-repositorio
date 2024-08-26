d76f3da11244be2bd1546093044dcd225c5c0d07
DANIELs1305 <daniel.cs25@hotmail.com>
2024-08-26 07:07:37 -0500

    se crea repositorio local por primera vez

---
+++ pruebagit.js
@@ -0,0 +1,6 @@
+//comandos de git
+// comando para crear la rama original  =  git init 
+// comando para revisar si hay cambios  = git status 
+// comando para guardar los cambios que se hicieron  =  git add .
+// comando para limpiar ventana  =  clear 
+// comando para guardar cambios en el repositorio  =  git commit -m "alguna nota en tercera persona"
